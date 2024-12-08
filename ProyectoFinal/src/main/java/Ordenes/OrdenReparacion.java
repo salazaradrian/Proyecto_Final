ccb818981dd4c913a57d32e9ac997e91d5469252
salazar95 <asalazar.rod95@outlook.com>
2024-12-08 08:56:59 -0600

    Orden Reparacion

--- ProyectoFinal/src/main/java/Ordenes/OrdenReparacion.java
+++ ProyectoFinal/src/main/java/Ordenes/OrdenReparacion.java
@@ -3,8 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Ordenes;
+import Database.Conexion;
 import Vehiculos.Vehiculo;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -12,20 +17,106 @@ import java.util.ArrayList;
  */
 public class OrdenReparacion {
     
-    protected int orden;
-    protected String estado;
-    protected Vehiculo vehiculo;
+    private int orden;
+    private String estado;
+    private Vehiculo vehiculo;
+    private String descripcionproblema;
     protected ArrayList<String> piezas;
 
-    public OrdenReparacion(int orden, String estado, Vehiculo vehiculo) {
+    public OrdenReparacion(int orden, String estado, Vehiculo vehiculo,String descripcionproblema) {
         this.orden = orden;
         this.estado = estado;
         this.vehiculo = vehiculo;
+        this.descripcionproblema = descripcionproblema;
     }
 
+    
+    //Metodos para el CRUD
+    Conexion conexion = new Conexion();
+     DefaultTableModel modelo = new DefaultTableModel(){
+            
+            // Hacer que la tabla no sea editable
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            } 
+        };
+    
+    
+    
+     
+     public void agregar(){
+         Conexion conexion = new Conexion();
+         
+         String sql = conexion.conectar().prepareCall(sql);
+         
+          cs.setString(1, this.marca);
+            cs.setString(2, this.tipo);
+            cs.setDouble(3, this.precioPorHora);
+            cs.setString(4, this.estado.toString());
+
+            cs.execute();
+
+            JOptionPane.showMessageDialog(null, "El resgistro se a guardado de manera exitosa");
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al agregar el registros");
+            System.out.println("Error Consulta: " + ex.toString());
+        } finally {
+            conexion.desconectar();
+        }
+  
+// METODO PARA ELIMINAR registros (DELETE)
+    public void eliminar() {
+        Conexion conexion = new Conexion();
+
+        String sql = "DELETE FROM ordenreparacion WHERE codigo = ?";
+        
+        try {
+            CallableStatement cs = conexion.conectar().prepareCall(sql);
+
+            cs.setInt(1, this.codigo);
+
+            cs.execute();
+
+            JOptionPane.showMessageDialog(null, "El resgistro se a eliminado de manera exitosa");
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al eliminar el registros");
+            System.out.println("Error Consulta: " + ex.toString());
+        } finally {
+            conexion.desconectar();
+        }
+         
+         
+         
+         
+     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     public int getOrden() {
         return orden;
     }
+
+
+public String getDescripcionproblema(){
+    return descripcionproblema;
+
+} 
+
+public String setDescripcionproblema(){
+    return descripcionproblema;
+
+}  
+    
 
     public String getEstado() {
         return estado;

c65ba62c181022f7fc86e0e00e48bbd0df6b0dde
roncloc <ronnypz.mcr@gmail.com>
2024-12-16 08:40:15 -0600

    Revert "Revert "ordenes""
    This reverts commit 9c0aade10a73c7467a2691ab66936ad1408a2c48.

--- ProyectoFinal/src/main/java/Vehiculos/Vehiculo.java
+++ ProyectoFinal/src/main/java/Vehiculos/Vehiculo.java
@@ -4,23 +4,65 @@
  */
 package Vehiculos;
 
+import Database.Conexion;
 import Usuarios.Usuario;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author rperez
  */
-public abstract class Vehiculo {
-    
+public class Vehiculo {
+
     protected String marca;
     protected String modelo;
     protected String placa;
-    protected Usuario usuario;
+    protected Tipo tipo;
+    protected int id;
 
-    public Vehiculo(String marca, String modelo, String placa) {
+    public Vehiculo(String marca, String modelo, String placa, Tipo tipo) {
         this.marca = marca;
         this.modelo = modelo;
         this.placa = placa;
+        this.tipo = tipo;
+    }
+
+  
+
+    public int agregar() {
+        String sql = "INSERT INTO vehiculo (marca, modelo, placa, tipoVehiculo) VALUES (?, ?, ?, ?)";
+        Conexion conexion = new Conexion();
+        int generatedId = -1; // Default value for failure
+
+        try {
+            PreparedStatement ps = conexion.conectar().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, this.marca);
+            ps.setString(2, this.modelo);
+            ps.setString(3, this.placa);
+            ps.setString(4, this.tipo.toString()); // Convert enum to String
+
+            ps.executeUpdate();
+
+            // Retrieve the generated keys (ID)
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                generatedId = rs.getInt(1); // Get the first column, which is the ID
+                this.id = generatedId; // Set the ID for the current object
+                JOptionPane.showMessageDialog(null, "Vehículo agregado exitosamente con ID: " + generatedId);
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al agregar vehículo.");
+            System.out.println("Error: " + ex);
+        } finally {
+            conexion.desconectar();
+        }
+
+        return generatedId; // Return the generated ID
     }
 
     public String getMarca() {
@@ -31,12 +73,24 @@ public abstract class Vehiculo {
         return modelo;
     }
 
-    public String getPlaca() {
-        return placa;
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
     }
 
-    public Usuario getUsuario() {
-        return usuario;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPlaca() {
+        return placa;
     }
 
     public void setMarca(String marca) {
@@ -51,14 +105,4 @@ public abstract class Vehiculo {
         this.placa = placa;
     }
 
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-    
-    
-    public abstract void actualizarUsuario();
-    public abstract void actualzarubicacion();
-    public abstract void obtenerubicacion();
-    
-    
 }
